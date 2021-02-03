e24f9293a1c0e176508508e55b29d1f2bc0bad69
Your Name <you@example.com>
2021-02-03 14:08:40 +0100

    recup classe et matiere Eleve -OK-

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,22 +9,30 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+
     <div id="app">
+
+
+
+
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <div><img src="/logo/Class'Up.png" style="max-height:50px;" alt=""></div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,44 +45,82 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto ">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <li class="nav-item  pr-3">
+                         <button type="submit" style="border-radius:12px 0 12px 0;background: #478bf9;border:none;
+                         font:bold 12px Verdana; padding:6px 0 6px 0;">
+                                <a class="nav-link" style="color:#fff" href="{{ route('login') }}">SE CONNECTER</a>
+                                  </button>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                       <button type="submit" style="border-radius:12px 0 12px 0;background:#478bf9 ;border:none;font:bold 12px Verdana; padding:6px 0 6px 0;">
+                                    <a class="nav-link"  style="color:#fff" href="{{ route('register') }}"> S'INSCRIRE</a>
+                                       </button>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                            <div class="nav-item dropdown">
+                                <button type="button" class="btn btn-primary rounded-pill dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color:#fff ;font-family: cursive">
+                                    {{ Auth::user()->prenom }} <span class="caret"></span>
                                 </a>
-
+                                </button>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <!--
+                                    <a class="dropdown-item" href="{{-- route('profile.show',Auth::user()) --}}">
+                                        Mon profil
+                                    </a>
+                                    -->
+                                    <!--
+                                    <a class="dropdown-item" href="{{-- route('classe',Auth::user()) --}}">
+                                        Ma classe
+                                    </a>
+                                    -->
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                       {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
                                         @csrf
+
                                     </form>
+
+
+                                    @can('manage-users')
+                                    <a class="dropdown-item" href="{{ route('admin.users.index') }}">
+                                        <button type="button" class="btn btn-success">
+                                         Gestion générale
+                                        </button>
+                                    </a>
+                                    @endcan
                                 </div>
-                            </li>
+
+
+
+                            </div>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
+
+          <main class="p-4">
+
+           {{--notification--}}
+           {{--@include('sweetalert::alert')--}}
             @yield('content')
-        </main>
-    </div>
+
+            </main>
+        </div>
+
+
+
 </body>
 </html>
